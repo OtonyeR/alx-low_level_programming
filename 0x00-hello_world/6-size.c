df096c5a25b5612a2bb06beb5a29c603841624d5
Otonye <otonye.robinson@cu.edu.ng>
2022-09-08 02:28:46 -0700

    A C program that evaluates the size of various types

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -3,7 +3,7 @@
 /**
  * main - Entry point
  *
- * Description - A C program that prints the size of various types on the computer it i compiled and run on.
+ * Description - A C program that prints the size of various types.
  *
  * Return: Always 0 (Success)
  *
